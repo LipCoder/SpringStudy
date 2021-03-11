e1f691575ffc78cad4fc8bae64232fede4c9bc9f
Lipcoder <sb921204@naver.com>
2021-03-11 23:23:11 +0900

    20210311
    1. 프레젠테이션 계층 CRUD
    2. Controller 정의
    3. WAS를 대체하는 테스트 방식 (mvcMock 사용)

---
+++ ex02/src/main/java/org/zerock/controller/BoardController.java
@@ -0,0 +1,73 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.domain.BoardVO;
+import org.zerock.service.BoardService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller		// Spring bean으로 인식할 수 있게
+@RequestMapping("/board/")	// '/board'로 시작하는 모든 처리를 여기서 하도록 지정
+@AllArgsConstructor
+@Log4j
+public class BoardController {
+	
+	private BoardService service;
+	
+	@GetMapping("/list")
+	public void list(Model model) {
+		
+		log.info("list");
+		model.addAttribute("list", service.getList());
+		
+	}
+	
+	@PostMapping("/register")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+		
+		log.info("register: " + board);
+		
+		service.register(board);
+		
+		rttr.addFlashAttribute("result", board.getBno());
+		
+		return "redirect:/board/list";
+		
+	}
+	
+	// @RequestParam을 사용해 bno값을 좀 더 명시적으로 처리한다.
+	// 파라미터 이름과 변수 이름을 기준으로 동작하기 때문에 이 어노테이션은 생략 가능
+	@GetMapping("/get")
+	public void get(@RequestParam("bno") Long bno, Model model) {
+		
+		log.info("/get");
+		model.addAttribute("board", service.get(bno));
+	}
+	
+	@PostMapping("/modify")
+	public String modify(BoardVO board, RedirectAttributes rttr) {
+		log.info("modify: " + board);
+		
+		if(service.modify(board)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		
+		return "redirect:/board/list";
+	}
+	
+	@PostMapping("/remove")
+	public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
+		log.info("remove..." + bno);
+		if(service.remove(bno)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		return "redirect:/board/list";
+	}
+}
